ea60bb49c324e33774a24c1f832407f0a2b19072
Aleazar yilma <yilma4060@gmail.com>
2022-09-27 13:18:05 +0300

    style rockets page and add reducer to reserve

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
