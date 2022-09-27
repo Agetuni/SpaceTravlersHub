44736396418ee56b027c9c2da356c409ed721569
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-27 15:13:29 -0500

    Add Logo in the navbar

--- src/components/navigation/nav.js
+++ src/components/navigation/nav.js
@@ -1,9 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import './nav.css';
+import { GiEarthAmerica } from 'react-icons/gi';
 
 const Nav = () => (
   <nav className="Nav_Bar">
-    <h1>Space Travelers Hub</h1>
+    <div className="Nav_Bar_Header">
+      <GiEarthAmerica className="NavBarLogo App-logo" />
+      <h1 className="Nav_Bar_Title">Space Travelers Hub</h1>
+    </div>
     <div className="Nav_Bar_Container">
       <NavLink className="Nav_Bar_Links" to="/Rockets">Rockets </NavLink>
       <NavLink className="Nav_Bar_Links" to="/missions">Missions </NavLink>
